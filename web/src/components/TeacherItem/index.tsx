8621be1223e87aeec0082b11043153e3277180dd
d3xt3er <48564999+d3xt3er@users.noreply.github.com>
2020-08-06 15:00:32 -0300

    NLW#2 Aula 3
    A escolha da stack + Front-End Conectado ao Back-End

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,36 +1,56 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem(){
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    
+   function createNewConnection() {
+       api.post('connections', {
+           user_id: teacher.id,
+       })
+   }
+    
     return (
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars2.githubusercontent.com/u/48564999?s=460&u=fddd39a54df01d4e78ac46d9ed52fabc8bb248fc&v=4" alt="Paulo"/>
+                        <img src={teacher.avatar} alt={teacher.name}/>
                         <div>
-                            <strong>Paulo José</strong>
-                            <span>Física</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
 
                     <p>
-                        Um dos melhores desenvolvedores de back-end com node.js
-                        <br></br>
-                        Nas horas vagas professor de uma disciplina que explica como o mundo funciona e suas reaçoes mais loucas
+                        {teacher.bio}
                     </p>
 
                     <footer>
                         <p>
                             Preço/Hora
-                            <strong>R$ 80,00</strong>
+                            <strong>R$ {teacher.cost}</strong>
                         </p>
 
-                        <button type="button">
+                        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                             <img src={whatsappIcon} alt="WhatsApp"/>
                             Entrar em contato
-                        </button>
+                        </a>
                     </footer>
                </article>
     );
